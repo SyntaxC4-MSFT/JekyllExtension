d69e53ea24897d84622733a40f4c2b587c131bef
Cory Fowler <cfowler@microsoft.com>
2016-11-29 12:31:06 -0800

    updating webjob with the proper destination path

--- Content/Hooks/jekyll-build/run.cmd
+++ Content/Hooks/jekyll-build/run.cmd
@@ -1,5 +1,5 @@
 IF EXIST %HOME%/site/repository/_config.yml (
 
-    call jekyll build -s "%HOME%/site/repository" -d "%DEPLOYMENT_TARGET%" --config %HOME%/site/repository/_config.yml,%HOME%/siteextensions/JekyllExtension/Hooks/save_webjobs.yml
+    call jekyll build -s "%HOME%/site/repository" -d "%HOME%/site/wwwroot" --config %HOME%/site/repository/_config.yml,%HOME%/siteextensions/JekyllExtension/Hooks/save_webjobs.yml
 
 )
